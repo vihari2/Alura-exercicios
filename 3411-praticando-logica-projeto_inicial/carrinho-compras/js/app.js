75c1b9f940541646f1e6a1abcbf4e132294de181
vik <freitasvitoria712@gmail.com>
2026-01-21 12:08:00 -0300

    [add]

--- 3411-praticando-logica-projeto_inicial/carrinho-compras/js/app.js
+++ 3411-praticando-logica-projeto_inicial/carrinho-compras/js/app.js
@@ -1,3 +1,7 @@
+
+let totalGeral;
+limpar();
+
 function adicionar() { 
     //recuperar os valores do formulário, NOME, QUANTIDADE E VALOR
     //calcular o preço, o nosso subtotal
@@ -7,18 +11,26 @@ function adicionar() {
     let produto = document.getElementById('produto').value;
     let nomeProduto = produto.split('-')[0];
     let valorUnitario = produto.split('R$')[1]; 
-    let quantidade = document.getElementById('quantidade'); 
+    let quantidade = document.getElementById('quantidade').value; 
+    let preco = quantidade * valorUnitario;
 
-    alert(nomeProduto);
-    alert(valorUnitario);
-    alert(quantidade.value);
-    
-    let preco = quantidade.value * valorUnitario;
-
-    alert(preco);
+    let carrinho = document.getElementById('lista-produtos');
+    carrinho.innerHTML = carrinho.innerHTML + ` <section class="carrinho__produtos__produto">
+          <span class="texto-azul">${quantidade}x </span>${nomeProduto}<span class="texto-azul">${preco} </span>
+        </section>`;
+    totalGeral = totalGeral + preco;
+    let campoTotal = document.getElementById('valor-total'); 
+    campoTotal.textContent = `R$  ${totalGeral}`;
+    document.getElementById('quantidade').value = 0;
+    //👉 “vá até o campo do formulário com id quantidade
+    //👉 e altere o valor que aparece nele”
+    //Variável JS não controla input HTML.
+    //Input HTML só muda mexendo no DOM.
 } 
 
 
 function limpar() {
-
+totalGeral = 0
+document.getElementById('lista-produtos').innerHTML = '';
+document.getElementById('valor-total').textContent = 'R$ 0';
 }
